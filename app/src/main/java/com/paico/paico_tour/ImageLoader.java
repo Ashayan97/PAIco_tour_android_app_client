ae78cddb719b05ef58175624203c1e29f7873b8c
Ashayan97 <shayan_stu@yahoo.com>
2020-04-10 23:59:31 +0430

    new loader

--- app/src/main/java/com/paico/paico_tour/ImageLoader.java
+++ app/src/main/java/com/paico/paico_tour/ImageLoader.java
@@ -3,19 +3,21 @@ package com.paico.paico_tour;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.provider.ContactsContract;
 import android.widget.ImageView;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class ImageLoader extends AsyncTask<String, Void, Bitmap> {
+public class ImageLoader extends AsyncTask<String, Void, Void> {
     private static ImageLoader imageLoader;
 
-    public static ImageLoader getInstance() {
+    public ImageView imageView;
+
+    public static ImageLoader getInstance(ImageView imageView) {
         if (imageLoader == null)
             imageLoader = new ImageLoader();
+        imageLoader.imageView=imageView;
         return imageLoader;
     }
 
@@ -23,21 +25,19 @@ public class ImageLoader extends AsyncTask<String, Void, Bitmap> {
     }
 
     @Override
-    protected Bitmap doInBackground(String... strings) {
-        ImageView view;
+    protected Void doInBackground(String... strings) {
         Bitmap bmp = null;
         URL url = null;
         try {
             url = new URL(strings[0]);
             bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-
+            imageView.setImageBitmap(bmp);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return bmp;
+        return null;
     }
 
 }
