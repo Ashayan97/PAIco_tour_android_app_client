ab4eeeb6c0c0a69bc7285bfa2aa0dd84d4e4659a
Ashayan97 <shayan_stu@yahoo.com>
2020-04-27 23:03:19 +0430

    adding dexter and cameraView libraries :)

---
+++ app/src/main/java/com/paico/paico_tour/BarCodeScannigFragment.java
@@ -0,0 +1,12 @@
+package com.paico.paico_tour;
+
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+
+public class BarCodeScannigFragment {
+    FirebaseVisionBarcodeDetectorOptions options =
+            new FirebaseVisionBarcodeDetectorOptions.Builder()
+                    .setBarcodeFormats(
+                            FirebaseVisionBarcode.FORMAT_QR_CODE)
+                    .build();
+}
