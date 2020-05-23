62577a6a0bdf6c09bbb5f0e1d5d31e400aad6205
Ashayan97 <shayan_stu@yahoo.com>
2020-05-23 22:41:11 +0430

    some of the barcode reader finished :|

--- app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
+++ app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
@@ -1,23 +1,60 @@
 package com.paico.paico_tour;
 
+import android.Manifest;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class BarcodeReaderFragment extends Fragment {
-    @Nullable
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+
+public class BarcodeReaderFragment extends AppCompatActivity {
+    private CameraView cameraView;
+    private boolean isDetected;
+    private Button startScanning;
+    private FirebaseVisionBarcodeDetectorOptions options;
+    private FirebaseVisionBarcodeDetector detector;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.barcode_fragment,container,false);
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_barcode);
+
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    private void getCameraPermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                setUpCamera();
+            }
+
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(BarcodeReaderFragment.this, "we need your permission to use camera for your payment", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
+
     }
+
+    private void setUpCamera() {
+    }
+
 }
