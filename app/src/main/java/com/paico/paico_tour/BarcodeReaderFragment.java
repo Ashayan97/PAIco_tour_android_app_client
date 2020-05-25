0efdcc9d7078e1bbb6d76d476be57ffe093607a6
Ashayan97 <shayan_stu@yahoo.com>
2020-05-25 17:24:59 +0430

    completing BarcodeReaderFragment :)

--- app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
+++ app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
@@ -3,14 +3,19 @@ package com.paico.paico_tour;
 import android.Manifest;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -18,11 +23,13 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
 
 public class BarcodeReaderFragment extends AppCompatActivity {
     private CameraView cameraView;
     private boolean isDetected;
-    private Button startScanning;
+    private ImageButton startScanning;
     private FirebaseVisionBarcodeDetectorOptions options;
     private FirebaseVisionBarcodeDetector detector;
 
@@ -55,6 +62,37 @@ public class BarcodeReaderFragment extends AppCompatActivity {
     }
 
     private void setUpCamera() {
+        startScanning = findViewById(R.id.activity_barcode_start_scan_btn);
+        startScanning.setEnabled(isDetected);
+        startScanning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isDetected = !isDetected;
+            }
+        });
+        cameraView.findViewById(R.id.barcode_fragment_camera_view);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+                processImage(getVisionImageFromFrame(frame));
+            }
+        });
     }
+
+    private void processImage(FirebaseVisionImage image) {
+
+    }
+
+    public FirebaseVisionImage getVisionImageFromFrame(Frame frame){
+        byte[] data=frame.getData();
+        FirebaseVisionImageMetadata metadata=new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setHeight(frame.getSize().getHeight())
+                .setWidth(frame.getSize().getWidth())
+                .build();
+        return FirebaseVisionImage.fromByteArray(data,metadata);
+    }
+
 
 }
