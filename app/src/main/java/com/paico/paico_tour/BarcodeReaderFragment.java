9c595539bafc57c57526fce3256ef6dcf8dcf100
Ashayan97 <shayan_stu@yahoo.com>
2020-05-26 10:51:32 +0430

    finishing qr code scanner :))

--- app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
+++ app/src/main/java/com/paico/paico_tour/BarcodeReaderFragment.java
@@ -2,6 +2,8 @@ package com.paico.paico_tour;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
@@ -17,16 +19,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.vision.Detector;
+import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.frame.Frame;
@@ -49,24 +54,18 @@ public class BarcodeReaderFragment extends AppCompatActivity {
     }
 
     private void getCameraPermission() {
-        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                setUpCamera();
-            }
+        Dexter.withContext(this).withPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO})
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        setUpCamera();
+                    }
 
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
 
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                Toast.makeText(BarcodeReaderFragment.this, "we need your permission to use camera for your payment", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-
-            }
-        }).check();
-
+                    }
+                }).check();
     }
 
     private void setUpCamera() {
@@ -86,10 +85,14 @@ public class BarcodeReaderFragment extends AppCompatActivity {
                 processImage(getVisionImageFromFrame(frame));
             }
         });
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+                .build();
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
     }
 
     private void processImage(FirebaseVisionImage image) {
-        if (!isDetected){
+        if (!isDetected) {
             detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
                 @Override
                 public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
@@ -98,7 +101,7 @@ public class BarcodeReaderFragment extends AppCompatActivity {
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(BarcodeReaderFragment.this,""+e.getMessage(),Toast.LENGTH_LONG);
+                    Toast.makeText(BarcodeReaderFragment.this, "" + e.getMessage(), Toast.LENGTH_LONG);
                 }
             });
 
@@ -108,14 +111,34 @@ public class BarcodeReaderFragment extends AppCompatActivity {
     }
 
     private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
-        if(firebaseVisionBarcodes.size()>0){
-            isDetected=true;
+        if (firebaseVisionBarcodes.size() > 0) {
+            isDetected = true;
             startScanning.setEnabled(isDetected);
-            for (FirebaseVisionBarcode item:firebaseVisionBarcodes){
+            for (FirebaseVisionBarcode item : firebaseVisionBarcodes) {
                 int value_type = item.getValueType();
-                switch (value_type){
+                switch (value_type) {
                     case FirebaseVisionBarcode.TYPE_TEXT:
                         createDialog(item.getRawValue());
+                        break;
+                    case FirebaseVisionBarcode.TYPE_URL:
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                        startActivity(intent);
+                        break;
+                    case FirebaseVisionBarcode.TYPE_CONTACT_INFO:
+                        String info = new StringBuilder("Name: ")
+                                .append(item.getContactInfo().getName().getFormattedName())
+                                .append("\n")
+                                .append("Address: ")
+                                .append(item.getContactInfo().getAddresses().get(0).getAddressLines())
+                                .append("\n")
+                                .append("Email: ")
+                                .append(item.getContactInfo().getEmails().get(0).getAddress())
+                                .toString();
+                        createDialog(info);
+                        break;
+                    default:
+                        break;
+
                 }
             }
         }
@@ -129,18 +152,18 @@ public class BarcodeReaderFragment extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
-        AlertDialog dialog=builder.create();
+        AlertDialog dialog = builder.create();
         dialog.show();
     }
 
-    public FirebaseVisionImage getVisionImageFromFrame(Frame frame){
-        byte[] data=frame.getData();
-        FirebaseVisionImageMetadata metadata=new FirebaseVisionImageMetadata.Builder()
+    public FirebaseVisionImage getVisionImageFromFrame(Frame frame) {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
                 .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
                 .setHeight(frame.getSize().getHeight())
                 .setWidth(frame.getSize().getWidth())
                 .build();
-        return FirebaseVisionImage.fromByteArray(data,metadata);
+        return FirebaseVisionImage.fromByteArray(data, metadata);
     }
 
 
