1dc51c29061a516111875d65f29a0bf9eef1a02c
shayan <sshayan1997@gmail.com>
2020-06-15 22:31:54 +0430

    SettingFragment.java almost done

--- app/src/main/java/com/paico/paico_tour/SettingFragment.java
+++ app/src/main/java/com/paico/paico_tour/SettingFragment.java
@@ -10,16 +10,19 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SettingFragment extends Fragment {
     private EditText fullName;
-    private EditText Password;
+    private EditText password;
     private EditText phone;
-    private EditText Date;
-    private EditText Email;
+    private EditText date;
+    private EditText email;
 
     private Button submit;
     private Button edit;
+
+    private RecyclerView gallery;
 
     @Nullable
     @Override
@@ -30,11 +33,45 @@ public class SettingFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        onClick(view);
+        findView(view);
+        onClick();
 
     }
 
-    private void onClick(View view) {
-//        fullName=view.findViewById();
+    private void onClick() {
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                edit.setClickable(true);
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                fullName.setClickable(true);
+                password.setClickable(true);
+                date.setClickable(true);
+                phone.setClickable(true);
+                email.setClickable(true);
+                submit.setClickable(true);
+                edit.setClickable(false);
+            }
+        });
+    }
+
+    private void findView(View view) {
+        fullName=view.findViewById(R.id.setting_fragment_name);
+        password = view.findViewById(R.id.setting_fragment_password);
+        phone=view.findViewById(R.id.setting_fragment_phone);
+        date=view.findViewById(R.id.setting_fragment_date);
+        email=view.findViewById(R.id.setting_fragment_email);
+
+        submit=view.findViewById(R.id.setting_fragment_submit_btn);
+        edit=view.findViewById(R.id.setting_fragment_edit_btn);
+
+        gallery=view.findViewById(R.id.setting_fragment_gallery);
     }
 }
