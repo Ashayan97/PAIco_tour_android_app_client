667213e1a0554f74634cc52d4c9d18032da79bb7
shayan <sshayan1997@gmail.com>
2020-06-20 23:41:20 +0430

    linear click sets :))

--- app/src/main/java/com/paico/paico_tour/ItemAndPlacesCardViewHandler.java
+++ app/src/main/java/com/paico/paico_tour/ItemAndPlacesCardViewHandler.java
@@ -1,9 +1,11 @@
 package com.paico.paico_tour;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -19,8 +21,10 @@ import java.util.ArrayList;
 public class ItemAndPlacesCardViewHandler extends RecyclerView.Adapter<ItemAndPlacesCardViewHandler.ViewHolder> {
 
     private ArrayList<Item> items;
+    private Context context;
 
-    public ItemAndPlacesCardViewHandler(ArrayList<Item> items) {
+    public ItemAndPlacesCardViewHandler(ArrayList<Item> items, Context context) {
+        this.context=context;
         if (items == null)
             items = new ArrayList<>();
         else
@@ -35,12 +39,20 @@ public class ItemAndPlacesCardViewHandler extends RecyclerView.Adapter<ItemAndPl
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.placeName.setText(items.get(position).getTitle());
         holder.info.setText(items.get(position).getInfo());
         holder.point.setText(String.valueOf(items.get(position).getPoint()));
         holder.ratingBar.setRating(items.get(position).getPoint());
         new ImageLoader(holder.placeImage,holder.progressBar).doInBackground(items.get(position).getImgProfile());
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ItemAndPlacesDialogBox dialogBox= new ItemAndPlacesDialogBox(context,items.get(position));
+                dialogBox.show();
+            }
+        });
 
     }
 
@@ -56,6 +68,7 @@ public class ItemAndPlacesCardViewHandler extends RecyclerView.Adapter<ItemAndPl
         public ImageView placeImage;
         public RatingBar ratingBar;
         public ProgressBar progressBar;
+        public LinearLayout layout;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -65,6 +78,7 @@ public class ItemAndPlacesCardViewHandler extends RecyclerView.Adapter<ItemAndPl
             placeImage = itemView.findViewById(R.id.item_and_places_image_view);
             ratingBar = itemView.findViewById(R.id.item_and_places_rating_bar);
             info = itemView.findViewById(R.id.item_and_places_place_description);
+            layout = itemView.findViewById(R.id.item_and_places_layout);
 
         }
 
