f24acc4144003b64df53e28cdbf8f1b3ee2409b8
shayan <sshayan1997@gmail.com>
2020-06-21 22:07:19 +0430

    PlaceActivity.java item button set

--- app/src/main/java/com/paico/paico_tour/PlaceActivity.java
+++ app/src/main/java/com/paico/paico_tour/PlaceActivity.java
@@ -1,5 +1,6 @@
 package com.paico.paico_tour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -81,7 +82,8 @@ public class PlaceActivity extends AppCompatActivity {
         placesAndItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO
+                Intent intent=new Intent(PlaceActivity.this,ItemAndPlaceActivity.class);
+                startActivity(intent);
             }
         });
         givePoint.setOnClickListener(new View.OnClickListener() {
