5648cb662adf8bfbaada51b12f549f924c493cd1
shayan <sshayan1997@gmail.com>
2020-07-23 20:56:12 +0430

    completing sign up :))

--- app/src/main/java/com/paico/paico_tour/SignupFragment.java
+++ app/src/main/java/com/paico/paico_tour/SignupFragment.java
@@ -1,6 +1,7 @@
 package com.paico.paico_tour;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +13,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class SignupFragment extends Fragment {
 
     private EditText firstName;
     private EditText lastName;
-    private EditText username;
     private EditText password;
     private EditText email;
     private EditText phoneNumber;
-    private EditText birthDay;
     private Button confirmBtn;
     private Finisher finisher;
+    private FirebaseAuth mAuth;
+
 
     public SignupFragment(Finisher finisher) {
         this.finisher = finisher;
@@ -40,6 +48,7 @@ public class SignupFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         findView(view);
+        mAuth = FirebaseAuth.getInstance();
         onClick();
     }
 
@@ -47,12 +56,43 @@ public class SignupFragment extends Fragment {
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkNotNull(username, "username") && checkNotNull(password, "password") && checkNotNull(firstName, "first name") && checkNotNull(lastName, "last name") &&
+                if (checkNotNull(password, "password") && checkNotNull(firstName, "first name") && checkNotNull(lastName, "last name") &&
                         checkNotNull(email, "email") &&
-                      //  checkNotNull(birthDay, "birth day") &&
                         checkNotNull(phoneNumber, "phone number")) {
-                    //TODO
+                    String sEmail = email.getText().toString();
+                    String sPassword = password.getText().toString();
+                    String sPhoneNumber= phoneNumber.getText().toString();
+                    final String sFirstName=firstName.getText().toString();
+                    String sLastName=lastName.getText().toString();
+                    mAuth.createUserWithEmailAndPassword(sEmail, sPassword).
+                            addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("userCreation", "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                .setDisplayName(sFirstName)
+                                                .build();
 
+                                        user.updateProfile(profileUpdates)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+
+                                                        }
+                                                    }
+                                                });
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("userCreation", "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(getContext(), "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                     ConfrmationCodeDialogBox confrmationCodeDialogBox = new ConfrmationCodeDialogBox(getActivity(),finisher);
                     confrmationCodeDialogBox.show();
 
@@ -73,11 +113,9 @@ public class SignupFragment extends Fragment {
     private void findView(View view) {
         firstName = view.findViewById(R.id.sign_up_first_name);
         lastName = view.findViewById(R.id.sign_up_last_name);
-        username = view.findViewById(R.id.sign_up_username);
         password = view.findViewById(R.id.sign_up_password);
         email = view.findViewById(R.id.sign_up_email);
         phoneNumber = view.findViewById(R.id.sign_up_phone_number);
-      //  birthDay = view.findViewById(R.id.sign_up_birth_day);
         confirmBtn = view.findViewById(R.id.sign_up_confirm_btn);
     }
 }
